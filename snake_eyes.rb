3f079cd42f7d07c6adad97033dd41484e849e70a
Andrew <andrew.harte@outlook.com>
2018-09-09 21:20:02 +0100

    Add exisitng file

--- snake_eyes.rb
+++ snake_eyes.rb
@@ -1,10 +1,53 @@
-puts "How many times would you like to roll the dice? "
-answer = gets.chomp.to_i
-number = answer.times.map{Random.rand(6)}
-print number
-selections = Array.new
-selections << number
-#selections = selections.map!(&: to_i)
-sum = 0
-output = selections.inject(0, :+)
-puts output
+class SnakeEyes
+
+	attr_reader :side, :answer
+	
+	def initialize
+		@side = side
+		@answer = answer
+	end
+
+	def side
+		side = (Random.rand(6))
+		#puts side
+	end
+
+	def roll(input)
+		puts "How many times would you like to roll the dice? "
+		answer = input
+		puts answer
+	end
+
+	def result
+		number = answer.times.map{@side}
+		print number
+	end
+
+
+	# def dice(times)
+	# 	puts "How many times would you like to roll the dice? "
+	# 	answer = times
+	# 	puts answer.to_i
+	# 	number = answer.times.map{Random.rand(6)}
+	# 	print number
+	# 	output = number.inject(0, :+)
+	# 	puts output
+	# end
+
+end
+
+dice_roll = SnakeEyes.new
+dice_roll.roll(6)
+dice_roll.result
+#dice_roll.side
+# class CrownAndAnchor
+
+# 	require :SnakeEyes;
+
+
+# puts "How many times would you like to roll the dice? "
+# answer = gets.chomp.to_i
+# number = answer.times.map{Random.rand(6)}
+# print number
+# output = number.inject(0, :+)
+# puts output
