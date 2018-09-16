367be3302dd74b6471c8daf11241c3c3bf4dfac2
Andrew <andrew.harte@outlook.com>
2018-09-16 22:40:55 +0100

    Sunday 2240

--- snake_eyes.rb
+++ snake_eyes.rb
@@ -1,18 +1,24 @@
 class SnakeEyes
 
-	#attr_accessor input:
+	#attr_accessor :side
 
 	def initialize (input)
 		@input = input
+		@side = side
 	end
 
 	def roll
 		puts "You are rolling the dice #{@input} times"
+		number = Array.new
 		count = 0
 		until count == @input
-			puts side
+			number << side
 			count = count+1
 		end
+		print number
+		puts "\n"
+		output = number.inject(0, :+)
+		puts "Sum of rolls = #{output}"
 	end
 
 	def side
@@ -23,10 +29,17 @@ end
 
 
 class CrownAndAnchor < SnakeEyes
-	def side 
-		side = {1 => "crown", 2 => "anchor", 3 => "heart", 
-						4 => "club", 5 => "diamond", 6 => "spade"}
-	end
+	
+	def test
+	puts @side
+end
+	# def sideca_roll
+	# 	sideca = {1 => "crown", 2 => "anchor", 3 => "heart", 
+	#  					4 => "club", 5 => "diamond", 6 => "spade"}
+	# 	#sideca.each{|key, value| puts "#{key} is #{value}"}
+	# 	answer = @side
+	# 	answer.each{ |item| print sideca[item]}
+	# end
 	
 end
 
@@ -35,17 +48,17 @@ end
 	# 	puts "How many times would you like to roll the dice? "
 	# 	answer = times
 	# 	puts answer.to_i
-	# 	number = answer.times.map{Random.rand(6)}
-	# 	print number
-	# 	output = number.inject(0, :+)
-	# 	puts output
+		# number = answer.times.map{Random.rand(6)}
+		# print number
+		# output = number.inject(0, :+)
+		# puts output
 	# end
 
-# dice_roll = SnakeEyes.new(5)
-# dice_roll.roll
-crown_roll = CrownAndAnchor.new(2)
-crown_roll.roll
-
+dice_roll = SnakeEyes.new(3)
+dice_roll.roll
+#dice_roll.sum
+# crown_roll = CrownAndAnchor.new(2)
+# crown_roll.test
 
 # 	require :SnakeEyes;
 
